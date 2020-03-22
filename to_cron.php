62438e1f563f4397974e9655978af52fc5702e49
ALex.Liksagen <github-lxgn@ex.liksagen.com>
2020-03-22 14:10:27 +0300

    modify to cron

--- to_cron.php
+++ to_cron.php
@@ -4,6 +4,6 @@ $a = "nginx_node_upstream_checker.php";
 
 $d = __DIR__;
 $f = $d."/".$a;
-$txt = "* * * * * 	root	cd $d;./$a > $a.log 2 > &1\n";
+$txt = "* * * * * 	root	cd $d;./$a > $a.log 2>&1\n";
 
 print $txt;
